5df80dbe51949e6b222802778468b63c030716eb
Renato Gama <renatoargh@gmail.com>
2016-10-30 18:33:47 -0200

    heroku deploy

--- index.js
+++ index.js
@@ -1,63 +1,124 @@
 const async = require('async')
 const Github = require('github')
 const express = require('express')
-const npm = require('npm')
 const bodyParser = require('body-parser')
+const semver = require('semver')
+const request = require('request')
+const exec = require('child_process').exec
 
-// Setup express app
 const app = express()
 app.use(bodyParser.json())
 
-// Setup github api client
-const github = new Github()
-github.authenticate({
-  type: 'token',
-  token: process.env.GH_TOKEN
-});
+function getBadgeUrl ({subject, status, color }) {
+  return 'https://img.shields.io/badge/' + [
+    subject, status, color
+  ].join('-') + '.svg'
+}
 
-app.post('/commit', function (req, res, next) {
-  const payload = Object.freeze(req.body)
-  const repository = payload.repository
+function noop (returnValue) {
+  return function (owner, repo, callback) {
+    callback(null, returnValue)
+  }
+}
 
-  function downloadPackage(cb) {
-    github.repos.getContent({
-      owner: repository.owner.login,
-      repo: repository.name,
-      path: 'package.json'
-    }, cb)
+const cache = {}
+app.get('/:owner/:repo', function (req, res, next) {
+  const { owner, repo } = req.params
+  const isOutdated = cache[req.path]
+  let fn = noop(isOutdated)
+
+  if (typeof isOutdated === 'undefined') {
+    fn = checkDependencies
   }
 
-  function parsePackage(file, cb) {
+  fn(owner, repo, function (err, isOutdated) {
+    if (err) {
+      return next(err)
+    }
+
+    const badgeUrl = getBadgeUrl({
+      subject: 'dependencies',
+      status: isOutdated ? 'outdated' : 'uptodate',
+      color: isOutdated ? 'red' : 'green'
+    })
+
+    request(badgeUrl).pipe(res)
+  })
+})
+
+function checkDependencies (owner, repo, callback) {
+  function downloadPackage(callback) {
+    const github = new Github()
+
+    github.authenticate({
+      type: 'token',
+      token: process.env.GH_TOKEN
+    })
+
+    github.repos.getContent({
+      owner: owner,
+      repo: repo,
+      path: 'package.json'
+    }, callback)
+  }
+
+  function parsePackage(file, callback) {
     let pkg = new Buffer(file.content, file.encoding)
     pkg = pkg.toString('ascii')
 
     try {
-      cb(null, JSON.parse(pkg))
+      callback(null, JSON.parse(pkg))
     } catch(err) {
-      cb(err)
+      callback(err)
     }
   }
 
-  function iterateThroughDependencies (pkg, cb) {
-    async.mapValues(pkg.dependencies, function (name, version, cb) {
-      npm.commands.info([name], function (err, info) {
-        console.log(info.name, info.description)
-        cb()
+  function getLatestVersions (pkg, callback) {
+    async.mapValues(pkg.dependencies, function (version, name, cb) {
+      exec(`npm info ${name}@latest version`, function (err, latest) {
+        if (err) {
+          return cb(err)
+        }
+
+        cb(null, {
+          current: version,
+          latest: latest.trim()
+        })
       })
-    }, cb)
+    }, callback)
+  }
+
+  function filterOutdated (packages, callback) {
+    const outdated = Object.keys(packages).filter(function (pkg) {
+      pkg = packages[pkg]
+      return !semver.satisfies(pkg.latest, pkg.current)
+    })
+
+    callback(null, !!outdated.length)
+  }
+
+  function setCache (isOutdated, cb) {
+    cache[`/${owner}/${repo}`] = isOutdated
+    cb(null, isOutdated)
   }
 
   async.waterfall([
     downloadPackage,
     parsePackage,
-    iterateThroughDependencies
-  ], function (err, pkg) {
+    getLatestVersions,
+    filterOutdated,
+    setCache
+  ], callback || function (err) {
     if (err) {
-      return next(err)
+      console.log(err)
     }
-
-    res.status(202).json(pkg)
   })
+}
+
+app.post('/commit', function (req, res, next) {
+  res.status(202).json({})
+  const repository = req.body.repository
+  checkDependencies(repository.owner.login, repository.name)
 })
 
 app.listen(process.env.PORT || 3000)
