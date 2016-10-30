d988b229dd2dac0f5de41e2c4a928efb1d907891
Renato Gama <renatoargh@gmail.com>
2016-10-30 16:45:42 -0200

    heroku deploy

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+const Github = require('github')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+// Setup express app
 const app = express()
 app.use(bodyParser.json())
 
+// Setup github api client
+const github = new GitHubApi()
+github.authenticate({
+  type: "token",
+  token: process.env.GH_TOKEN
+});
+
 app.post('/commit', function (req, res, next) {
   const payload = Object.freeze(req.body)
-  console.log(JSON.stringify(payload, null, 4))
-  res.status(202).json({})
+  const repository = payload.repository
+
+  github.repos.getContent({
+    owner: repository.owner.login,
+    repo: repository.name,
+    path: 'package.json'
+  }, function (err, pkg) {
+    if (err) {
+      return next(err)
+    }
+
+    console.log(JSON.stringify(pkg, null, 4))
+    res.status(202).json({})
+  })
 })
 
 app.listen(process.env.PORT || 3000)
